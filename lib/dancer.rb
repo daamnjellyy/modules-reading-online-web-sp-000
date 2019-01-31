fa21c8828f5769e1baa075e8f5c1f5f9f5d1a91e
DANIELLE PARK <dpark1008@gmail.com>
2019-01-31 19:56:58 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,6 +1,6 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
-require_relative 'fancy_dance.rb'
+require_relative './fancy_dance.rb'
 
 
 class Dancer
@@ -14,10 +14,6 @@ class Dancer
 end
 
 class Dancer
-  extend MetaDancing
-end
-
-class Dancer
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
-end 
+end
