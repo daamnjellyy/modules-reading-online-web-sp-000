dfa107dbdbc4e1128a8a7f7d04910897be100e60
DANIELLE PARK <dpark1008@gmail.com>
2019-01-31 19:58:10 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,6 +3,8 @@ require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
 
 class Kid
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
 
   attr_accessor :name
 
@@ -11,7 +13,3 @@ class Kid
   end
 end
 
-class Kid
-  extend FancyDance::ClassMethods
-  include FancyDance::InstanceMethods
-end
